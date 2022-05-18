47fe14385bc68d88ae7725ba095220a593e678ea
vovacelidze <louglescom@gmail.com>
2022-05-18 14:56:01 +0300

    lessons 1-2

---
+++ Lection-1/console-app.js
@@ -0,0 +1,14 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin, // ввод из стандартного потока
+  output: process.stdout, // вывод в стандартный поток
+})
+rl.on('line', (cmd) => {
+  console.log(`You just typed: ${cmd}`)
+});
+
+rl.question('Как вас зовут?', (answer) => {
+  console.log(`Приятно познакомиться ${answer}`)
+  rl.close();
+});
+
